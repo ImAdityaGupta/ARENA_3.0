280bed96f9f53405fde4115ef7fed422a412ccad
Aditya Gupta <adidh14@gmail.com>
2025-09-29 16:00:53 +0000

    update

--- chapter0_fundamentals/exercises/part2_cnns/0.2_my_attempt.py
+++ chapter0_fundamentals/exercises/part2_cnns/0.2_my_attempt.py
@@ -259,13 +259,13 @@ for epoch in range(epochs):
         pbar.set_postfix(epoch=f"{epoch + 1}/{epochs}", loss=f"{loss:.3f}")
 
 
-line(
-    loss_list,
-    x_max=epochs * len(mnist_trainset),
-    labels={"x": "Examples seen", "y": "Cross entropy loss"},
-    title="SimpleMLP training on MNIST",
-    width=700,
-)
+# line(
+#     loss_list,
+#     x_max=epochs * len(mnist_trainset),
+#     labels={"x": "Examples seen", "y": "Cross entropy loss"},
+#     title="SimpleMLP training on MNIST",
+#     width=700,
+# )
 
 @dataclass
 class SimpleMLPTrainingArgs:
@@ -318,13 +318,13 @@ args = SimpleMLPTrainingArgs()
 loss_list, model = train(args)
 
 
-line(
-    loss_list,
-    x_max=args.epochs * len(mnist_trainset),
-    labels={"x": "Examples seen", "y": "Cross entropy loss"},
-    title="SimpleMLP training on MNIST",
-    width=700,
-)
+# line(
+#     loss_list,
+#     x_max=args.epochs * len(mnist_trainset),
+#     labels={"x": "Examples seen", "y": "Cross entropy loss"},
+#     title="SimpleMLP training on MNIST",
+#     width=700,
+# )
 
 
 def train(args: SimpleMLPTrainingArgs) -> tuple[list[float], list[float], SimpleMLP]:
@@ -388,3 +388,335 @@ line(
     title="SimpleMLP training on MNIST",
     width=800,
 )
+
+
+
+class Conv2d(nn.Module):
+    def __init__(
+        self,
+        in_channels: int,
+        out_channels: int,
+        kernel_size: int,
+        stride: int = 1,
+        padding: int = 0,
+    ):
+        """
+        Same as torch.nn.Conv2d with bias=False.
+
+        Name your weight field `self.weight` for compatibility with the PyTorch version.
+
+        We assume kernel is square, with height = width = `kernel_size`.
+        """
+        super().__init__()
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+
+        # YOUR CODE HERE - define & initialize `self.weight`
+        self.weight = t.rand((out_channels, in_channels, kernel_size, kernel_size))
+        scale = 1 / np.sqrt(self.in_channels*self.kernel_size*self.kernel_size)
+        self.weight = nn.Parameter((self.weight * scale * 2) - scale)
+
+
+
+    def forward(self, x: Tensor) -> Tensor:
+        """Apply the functional conv2d, which you can import."""
+        return t.nn.functional.conv2d(x, self.weight, stride=self.stride, padding=self.padding)
+
+    def extra_repr(self) -> str:
+        keys = ["in_channels", "out_channels", "kernel_size", "stride", "padding"]
+        return ", ".join([f"{key}={getattr(self, key)}" for key in keys])
+
+
+tests.test_conv2d_module(Conv2d)
+m = Conv2d(in_channels=24, out_channels=12, kernel_size=3, stride=2, padding=1)
+print(f"Manually verify that this is an informative repr: {m}")
+
+class MaxPool2d(nn.Module):
+    def __init__(self, kernel_size: int, stride: int | None = None, padding: int = 1):
+        super().__init__()
+        self.kernel_size = kernel_size
+        self.stride = stride
+        self.padding = padding
+
+    def forward(self, x: Tensor) -> Tensor:
+        """Call the functional version of maxpool2d."""
+        return F.max_pool2d(
+            x, kernel_size=self.kernel_size, stride=self.stride, padding=self.padding
+        )
+
+    def extra_repr(self) -> str:
+        """Add additional information to the string representation of this class."""
+        return ", ".join(
+            [f"{key}={getattr(self, key)}" for key in ["kernel_size", "stride", "padding"]]
+        )
+    
+class Sequential(nn.Module):
+    _modules: dict[str, nn.Module]
+
+    def __init__(self, *modules: nn.Module):
+        super().__init__()
+        for index, mod in enumerate(modules):
+            self._modules[str(index)] = mod
+
+    def __getitem__(self, index: int) -> nn.Module:
+        index %= len(self._modules)  # deal with negative indices
+        return self._modules[str(index)]
+
+    def __setitem__(self, index: int, module: nn.Module) -> None:
+        index %= len(self._modules)  # deal with negative indices
+        self._modules[str(index)] = module
+
+    def forward(self, x: Tensor) -> Tensor:
+        """Chain each module together, with the output from one feeding into the next one."""
+        for mod in self._modules.values():
+            x = mod(x)
+        return x
+
+
+# a = Sequential(Linear(100,10), ReLU(), Linear(10,100))
+# print(a)
+
+
+
+class BatchNorm2d(nn.Module):
+    # The type hints below aren't functional, they're just for documentation
+    running_mean: Float[Tensor, "num_features"]
+    running_var: Float[Tensor, "num_features"]
+    num_batches_tracked: Int[Tensor, ""]  # This is how we denote a scalar tensor
+
+    def __init__(self, num_features: int, eps=1e-05, momentum=0.1):
+        """
+        Like nn.BatchNorm2d with track_running_stats=True and affine=True.
+
+        Name the learnable affine parameters `weight` and `bias` in that order.
+        """
+        super().__init__()
+        self.num_features = num_features
+        self.eps = eps
+        self.momentum = momentum
+
+        self.weight = nn.Parameter(t.ones(num_features))
+        self.bias = nn.Parameter(t.zeros(num_features))
+
+        self.register_buffer("running_mean", t.zeros(num_features))
+        self.register_buffer("running_var", t.ones(num_features))
+        self.register_buffer("num_batches_tracked", t.tensor(0))
+
+    def forward(self, x: Tensor) -> Tensor:
+        """
+        Normalize each channel.
+
+        Compute the variance using `torch.var(x, unbiased=False)`
+        Hint: you may also find it helpful to use the argument `keepdim`.
+
+        x: shape (batch, channels, height, width)
+        Return: shape (batch, channels, height, width)
+        """
+        if self.training:
+            means = einops.reduce(x, 'b c h w -> c', 'mean')
+            variances = einops.reduce(x, 'b c h w -> c', t.var)
+
+            self.running_mean = (1-self.momentum)*self.running_mean + self.momentum*means
+            self.running_var = (1-self.momentum)*self.running_var + self.momentum*variances
+            self.num_batches_tracked += 1
+        else:
+            means = self.running_mean
+            variances = self.running_var
+
+
+        means = einops.rearrange(means, 'c -> 1 c 1 1')
+        variances = einops.rearrange(variances, 'c -> 1 c 1 1')
+
+        weights = einops.rearrange(self.weight, 'c -> 1 c 1 1')
+        biases = einops.rearrange(self.bias, 'c -> 1 c 1 1')
+
+        return ((x-means) / t.sqrt(variances+self.eps))*weights + biases
+
+    def extra_repr(self) -> str:
+        raise NotImplementedError()
+
+
+tests.test_batchnorm2d_module(BatchNorm2d)
+tests.test_batchnorm2d_forward(BatchNorm2d)
+tests.test_batchnorm2d_running_mean(BatchNorm2d)
+
+
+
+
+class AveragePool(nn.Module):
+    def forward(self, x: Tensor) -> Tensor:
+        """
+        x: shape (batch, channels, height, width)
+        Return: shape (batch, channels)
+        """
+        return x.mean(dim=(2,3))
+
+
+tests.test_averagepool(AveragePool)
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_feats: int, out_feats: int, first_stride=1):
+        """
+        A single residual block with optional downsampling.
+
+        For compatibility with the pretrained model, declare the left side branch first using a
+        `Sequential`.
+
+        If first_stride is > 1, this means the optional (conv + bn) should be present on the right
+        branch. Declare it second using another `Sequential`.
+        """
+        super().__init__()
+        is_shape_preserving = (first_stride == 1) and (
+            in_feats == out_feats
+        )  # determines if right branch is identity
+
+        self.left = Sequential(
+            Conv2d(in_feats, out_feats, 3, first_stride, 1),
+            BatchNorm2d(out_feats),
+            ReLU(),
+            Conv2d(out_feats, out_feats, 3, 1, 1),
+            BatchNorm2d(out_feats),
+        )
+        if is_shape_preserving:
+            self.right = Sequential()
+        else:
+            self.right = Sequential(
+                Conv2d(in_feats, out_feats, 1, first_stride, 0),
+                BatchNorm2d(out_feats),
+
+            )
+        self.relu = ReLU()
+
+
+    def forward(self, x: Tensor) -> Tensor:
+        """
+        Compute the forward pass. If no downsampling block is present, the addition should just add
+        the left branch's output to the input.
+
+        x: shape (batch, in_feats, height, width)
+
+        Return: shape (batch, out_feats, height / stride, width / stride)
+        """
+        left_side = self.left(x)
+        right_side = self.right(x)
+
+        return self.relu(left_side+right_side)
+
+
+
+with t.inference_mode():   # or: with t.no_grad():
+    tests.test_residual_block(ResidualBlock)
+
+class BlockGroup(nn.Module):
+    def __init__(self, n_blocks: int, in_feats: int, out_feats: int, first_stride=1):
+        """
+        An n_blocks-long sequence of ResidualBlock where only the first block uses the provided
+        stride.
+        """
+        super().__init__()
+        self.blocks = Sequential(
+            ResidualBlock(in_feats, out_feats, first_stride),
+            *[ResidualBlock(out_feats, out_feats, 1) for _ in range(n_blocks-1)]
+        )
+
+    def forward(self, x: Tensor) -> Tensor:
+        """
+        Compute the forward pass.
+
+        x: shape (batch, in_feats, height, width)
+
+        Return: shape (batch, out_feats, height / first_stride, width / first_stride)
+        """
+        return self.blocks(x)
+
+with t.inference_mode():
+    tests.test_block_group(BlockGroup)
+
+
+class ResNet34(nn.Module):
+    def __init__(
+        self,
+        n_blocks_per_group=[3, 4, 6, 3],
+        out_features_per_group=[64, 128, 256, 512],
+        first_strides_per_group=[1, 2, 2, 2],
+        n_classes=1000,
+    ):
+        super().__init__()
+        out_feats0 = 64
+        self.n_blocks_per_group = n_blocks_per_group
+        self.out_features_per_group = out_features_per_group
+        self.first_strides_per_group = first_strides_per_group
+        self.n_classes = n_classes
+
+        # YOUR CODE HERE - define all components of resnet34
+        self.blocks = Sequential(
+            Conv2d(3, out_feats0, 7, 2, 3),
+            BatchNorm2d(out_feats0),
+            ReLU(),
+            # Linear(out_feats0, self.out_features_per_group[-1]),
+            MaxPool2d(3, 2),
+            BlockGroup(n_blocks_per_group[0], out_feats0, out_features_per_group[0], first_strides_per_group[0]),
+            *[BlockGroup(n_blocks_per_group[n+1], out_features_per_group[n], out_features_per_group[n+1], first_strides_per_group[n+1]) for n, i in enumerate(n_blocks_per_group[1:])],
+            AveragePool(),
+            Linear(self.out_features_per_group[-1], 1000)
+        )
+
+        # for n, i in enumerate(n_blocks_per_group[1:])
+
+    def forward(self, x: Tensor) -> Tensor:
+        """
+        x: shape (batch, channels, height, width)
+        Return: shape (batch, n_classes)
+        """
+        return self.blocks(x)
+
+
+my_resnet = ResNet34()
+
+# (1) Test via helper function `print_param_count`
+target_resnet = (
+    models.resnet34()
+)  # without supplying a `weights` argument, we just initialize with random weights
+utils.print_param_count(my_resnet, target_resnet)
+
+# (2) Test via `torchinfo.summary`
+print("My model:", torchinfo.summary(my_resnet, input_size=(1, 3, 64, 64)), sep="\n")
+print(
+    "\nReference model:",
+    torchinfo.summary(target_resnet, input_size=(1, 3, 64, 64), depth=2),
+    sep="\n",
+)
+
+
+
+def copy_weights(my_resnet: ResNet34, pretrained_resnet: models.resnet.ResNet) -> ResNet34:
+    """Copy over the weights of `pretrained_resnet` to your resnet."""
+
+    # Get the state dictionaries for each model, check they have the same number of parameters &
+    # buffers
+    mydict = my_resnet.state_dict()
+    pretraineddict = pretrained_resnet.state_dict()
+    assert len(mydict) == len(pretraineddict), "Mismatching state dictionaries."
+
+    # Define a dictionary mapping the names of your parameters / buffers to their values in the
+    # pretrained model
+    state_dict_to_load = {
+        mykey: pretrainedvalue
+        for (mykey, myvalue), (pretrainedkey, pretrainedvalue) in zip(
+            mydict.items(), pretraineddict.items()
+        )
+    }
+
+    # Load in this dictionary to your model
+    my_resnet.load_state_dict(state_dict_to_load)
+
+    return my_resnet
+
+
+pretrained_resnet = models.resnet34(weights=models.ResNet34_Weights.IMAGENET1K_V1).to(device)
+my_resnet = copy_weights(my_resnet, pretrained_resnet).to(device)
+print("Weights copied successfully!")
